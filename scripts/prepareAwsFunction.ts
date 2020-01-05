d75fe6bb108ff3fef3d4a64a64930c53165bdee5
Jeroen Claassens <support@favware.tech>
2020-01-06 00:28:09 +0100

    build: setup AWS deployment

--- scripts/prepareAwsFunction.ts
+++ scripts/prepareAwsFunction.ts
@@ -2,7 +2,7 @@ import { copyFileAtomic, remove } from 'fs-nextra';
 import { join } from 'path';
 
 const ASSETS_PACKAGE_JSON = join(__dirname, '..', 'assets', 'package.lambda.json');
-const AWS_DIR = join(__dirname, '..', 'functions', 'dexa');
+const AWS_DIR = join(__dirname, '..', 'dist');
 const AWS_PACKAGE_JSON = join(AWS_DIR, 'package.json');
 const AWS_TSBUILDINFO = join(AWS_DIR, 'tsconfig.tsbuildinfo');
 
