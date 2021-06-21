7e8f3a495cf91fc9b8eb0f9d0d0ccdf8ae3f9d1e
Jeroen Claassens <support@favware.tech>
2021-06-21 20:42:58 +0200

    test: change to v8 coverage provider

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types';
 // eslint-disable-next-line @typescript-eslint/require-await
 export default async (): Promise<Config.InitialOptions> => ({
   preset: 'ts-jest',
+  coverageProvider: 'v8',
   testEnvironment: 'node',
   testRunner: 'jest-circus/runner',
   testMatch: ['<rootDir>/__tests__/*.test.ts'],
@@ -11,5 +12,7 @@ export default async (): Promise<Config.InitialOptions> => ({
     'ts-jest': {
       tsconfig: '<rootDir>/__tests__/tsconfig.json'
     }
-  }
+  },
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: ['<rootDir>/__tests__/', '<rootDir>/src/index.ts']
 });
